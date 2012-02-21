17c3ffc3fb7cd6ee5a01dc34f378e1946befca50
Ajith <root@ubuntu.(none)>
2012-02-21 23:39:18 +0530

    Watchdog Timer

---
+++ wdt.c
@@ -0,0 +1,28 @@
+#include <msp430.h>
+#define LED1 BIT0
+
+__attribute__((interrupt(WDT_VECTOR)))
+
+void isr()
+{
+	P1OUT ^= LED1;
+}
+
+//initialize watchdog as interval timer
+void watchdog_init()
+{
+// clk = smclk/32768, interval timer mode
+// wdt enabled (ie, WDTHOLD = 0)
+	WDTCTL = WDTPW | WDTTMSEL | WDTCNTCL ;
+
+	IE1 = IE1 | WDTIE ;
+}
+
+main()
+{
+	P1DIR = LED1;
+
+	__enable_interrupt();
+	watchdog_init();
+	while(1);
+}
